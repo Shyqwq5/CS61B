2f78649d4e40f0231a595ca3f089118b7a35ff80
shyqw <shyqwq5@gmail.com>
2024-06-15 12:15:36 +0100

    submit lab2

--- lab2/Arithmetic/ArithmeticTest.java
+++ lab2/Arithmetic/ArithmeticTest.java
@@ -1,7 +1,8 @@
 package Arithmetic;
 
-import static org.junit.Assert.*;
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class ArithmeticTest {
 
--- lab2/IntList/SquarePrimesTest.java
+++ lab2/IntList/SquarePrimesTest.java
@@ -17,4 +17,26 @@ public class SquarePrimesTest {
         assertEquals("14 -> 15 -> 16 -> 289 -> 18", lst.toString());
         assertTrue(changed);
     }
+
+    @Test
+    public void testSquarePrimesSimple1() {
+        IntList lst = IntList.of(11, 11, 11, 11, 11);
+        boolean changed = IntListExercises.squarePrimes(lst);
+        assertEquals("121 -> 121 -> 121 -> 121 -> 121", lst.toString());
+        assertTrue(changed);
+    }
+    @Test
+    public void testSquarePrimesSimple2() {
+        IntList lst = IntList.of(1, 0, -1, 11, 11);
+        boolean changed = IntListExercises.squarePrimes(lst);
+        assertEquals("1 -> 0 -> -1 -> 121 -> 121", lst.toString());
+        assertTrue(changed);
+    }
+    @Test
+    public void testSquarePrimesSimple3() {
+        IntList lst = IntList.of(1, 0, -1, 11, 11,9);
+        boolean changed = IntListExercises.squarePrimes(lst);
+        assertEquals("1 -> 0 -> -1 -> 121 -> 121 -> 9", lst.toString());
+        assertTrue(changed);
+    }
 }
