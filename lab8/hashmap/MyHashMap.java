c3cd7cb95fb4aed33f958de00740127168bae24e
shyqw <shyqwq5@gmail.com>
2024-06-28 01:26:25 +0100

    1

--- lab8/hashmap/MyHashMap.java
+++ lab8/hashmap/MyHashMap.java
@@ -33,9 +33,7 @@ public class MyHashMap<K, V> implements Map61B<K, V> {
         }
     }
 
-    /**
-     * Constructors
-     */
+    /** Constructors */
     public MyHashMap() {
         buckets = createTable(size);
     }
@@ -50,7 +48,7 @@ public class MyHashMap<K, V> implements Map61B<K, V> {
      * The load factor (# items / # buckets) should always be <= loadFactor
      *
      * @param initialSize initial size of backing array
-     * @param maxLoad     maximum load factor
+     * @param maxLoad maximum load factor
      */
     public MyHashMap(int initialSize, double maxLoad) {
         this.size = initialSize;
@@ -67,19 +65,19 @@ public class MyHashMap<K, V> implements Map61B<K, V> {
 
     /**
      * Returns a data structure to be a hash table bucket
-     * <p>
+     *
      * The only requirements of a hash table bucket are that we can:
-     * 1. Insert items (`add` method)
-     * 2. Remove items (`remove` method)
-     * 3. Iterate through items (`iterator` method)
-     * <p>
+     *  1. Insert items (`add` method)
+     *  2. Remove items (`remove` method)
+     *  3. Iterate through items (`iterator` method)
+     *
      * Each of these methods is supported by java.util.Collection,
      * Most data structures in Java inherit from Collection, so we
      * can use almost any data structure as our buckets.
-     * <p>
+     *
      * Override this method to use different data structures as
      * the underlying bucket type
-     * <p>
+     *
      * BE SURE TO CALL THIS FACTORY METHOD INSTEAD OF CREATING YOUR
      * OWN BUCKET DATA STRUCTURES WITH THE NEW OPERATOR!
      */
@@ -90,7 +88,7 @@ public class MyHashMap<K, V> implements Map61B<K, V> {
     /**
      * Returns a table to back our hash table. As per the comment
      * above, this table can be an array of Collection objects
-     * <p>
+     *
      * BE SURE TO CALL THIS FACTORY METHOD WHEN CREATING A TABLE SO
      * THAT ALL BUCKET TYPES ARE OF JAVA.UTIL.COLLECTION
      *
@@ -106,15 +104,12 @@ public class MyHashMap<K, V> implements Map61B<K, V> {
 
     // TODO: Implement the methods of the Map61B Interface below
     // Your code won't compile until you do so!
-
-    /**
-     * Removes all of the mappings from this map.
-     */
+    /** Removes all of the mappings from this map. */
 
     private void resize(int tableSize) {
         size = tableSize;
         Collection<Node>[] table = createTable(tableSize);
-        for (Collection<Node> col : buckets) {
+        for (Collection<Node> col: buckets){
             for (Node node : col) {
                 int index = Math.floorMod(node.key.hashCode(), size);
                 table[index].add(node);
@@ -124,16 +119,14 @@ public class MyHashMap<K, V> implements Map61B<K, V> {
 
     }
 
-    public void clear() {
+    public void clear(){
         buckets = createTable(size);
         load = 0;
         keySet = new HashSet<>();
     }
 
-    /**
-     * Returns true if this map contains a mapping for the specified key.
-     */
-    public boolean containsKey(K key) {
+    /** Returns true if this map contains a mapping for the specified key. */
+    public boolean containsKey(K key){
         return keySet.contains(key);
     }
 
@@ -141,23 +134,17 @@ public class MyHashMap<K, V> implements Map61B<K, V> {
      * Returns the value to which the specified key is mapped, or null if this
      * map contains no mapping for the key.
      */
-    public V get(K key) {
+    public V get(K key){
 
-        if (keySet.contains(key)) {
+        if (keySet.contains(key)){
             int index = Math.floorMod(key.hashCode(), size);
             for (Node node : buckets[index]) {
-                if (key.equals(node.key)) {
-                    return node.value;
-                }
-            }
+                if (key.equals(node.key)){
+                    return node.value;}}
         }
-        return null;
-    }
-
-    /**
-     * Returns the number of key-value mappings in this map.
-     */
-    public int size() {
+        return null;}
+    /** Returns the number of key-value mappings in this map. */
+    public int size(){
         return load;
     }
 
@@ -166,31 +153,26 @@ public class MyHashMap<K, V> implements Map61B<K, V> {
      * If the map previously contained a mapping for the key,
      * the old value is replaced.
      */
-    public void put(K key, V value) {
+    public void put(K key, V value){
         int index = Math.floorMod(key.hashCode(), size);
-        if (keySet.contains(key)) {
-            for (Node node : buckets[index]) {
-                if (key.equals(node.key)) {
-                    node.value = value;
-                    return;
-                }
-            }
-        } else {
-            keySet.add(key);
-            buckets[index].add(createNode(key, value));
-            load += 1;
-            int loadFactor = load / size;
-            if (loadFactor > maxLoad) {
-                size = size * 2;
-                resize(size);
-            }
+        if (keySet.contains(key)){for (Node node : buckets[index])
+        {if (key.equals(node.key)){
+            node.value = value;
+            return;}
         }
+        }
+        else{ keySet.add(key);
+        buckets[index].add(createNode(key, value));
+        load +=1;
+        int loadFactor = load/size;
+        if (loadFactor > maxLoad){
+            size = size *  2;
+            resize(size);
+        }}
     }
 
-    /**
-     * Returns a Set view of the keys contained in this map.
-     */
-    public Set<K> keySet() {
+    /** Returns a Set view of the keys contained in this map. */
+    public Set<K> keySet(){
         return keySet;
     }
 
@@ -199,75 +181,64 @@ public class MyHashMap<K, V> implements Map61B<K, V> {
      * Not required for Lab 8. If you don't implement this, throw an
      * UnsupportedOperationException.
      */
-    public V remove(K key) {
-        throw new UnsupportedOperationException();
-        /*
-        if (keySet.contains(key)) {
-            int index = Math.floorMod(key.hashCode(), size);
-            for (Node node : buckets[index]) {
-                if (key.equals(node.key)) {
-                    buckets[index].remove(node);
-                    return node.value;
-                }
+    public V remove(K key){
+        if (keySet.contains(key)){
+        int index = Math.floorMod(key.hashCode(), size);
+        for (Node node : buckets[index]) {
+            if (key.equals(node.key)) {
+                keySet.remove(node.key);
+                buckets[index].remove(node);
+                return node.value;
             }
-        }
-        return null;*/
-
+        }}
+        return null;
+    }
 
     /**
      * Removes the entry for the specified key only if it is currently mapped to
      * the specified value. Not required for Lab 8. If you don't implement this,
      * throw an UnsupportedOperationException.
      */
-    }
-    public V remove(K key, V value) {
-        throw new UnsupportedOperationException();
-        /*if (keySet.contains(key)) {
+    public V remove(K key, V value){
+        if (keySet.contains(key)){
             int index = Math.floorMod(key.hashCode(), size);
             for (Node node : buckets[index]) {
                 if (key.equals(node.key)) {
                     if (node.value.equals(value)) {
+                        keySet.remove(node.key);
                         buckets[index].remove(node);
                         return node.value;
                     }
                 }
-            }
-        }
-        return null;*/
+            }}
+        return null;
     }
 
-    public Iterator<K> iterator() {
+    public Iterator<K> iterator(){
         return new hashiterator();
     }
 
     private class hashiterator implements Iterator<K> {
         int t = load;
-        int i = 0;
-
-        public boolean hasNext() {
-            if (t > 0) {
-                return true;
-            }
+        int i =0;
+        public boolean hasNext(){
+            if (t > 0){return true;}
             return false;
         }
-
-        public K next() {
-            t -= 1;
-            i += 1;
-            int t = 0;
-            for (Collection<Node> col : buckets) {
+        public K next(){t -=1;
+            i+=1;
+            int t=0;
+            for (Collection<Node> col: buckets){
                 for (Node node : col) {
-                    t += 1;
-                    if (t == i) {
-                        return node.key;
-                    }
-                }
-
-
+                    t+=1;
+                    if (t == i){return node.key;}
             }
+
+
+        }
 
             return null;
         }
 
-    }
+}
 }
